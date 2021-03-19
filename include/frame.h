244f55a183f064b7e1de51c2b1be7b21723f8514
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-03-19 12:57:21 +0100

    activating frame.h

--- include/frame.h
+++ include/frame.h
@@ -1,32 +1,32 @@
-// #ifndef FRAME_H
-// #define FRAME_H
-// 
-// #include <opencv2/opencv.hpp> // remove this
-// #include <k4a/k4a.h>
-// #include <vector>
-// 
-// #include "point.h"
-// 
-// class Frame {
-// public:
-//     k4a_image_t m_rgb;
-//     k4a_image_t m_depth;
-//     k4a_image_t m_xyTable;
-//     k4a_image_t m_pointcloud;
-//     k4a_capture_t m_capture = nullptr;
-// 
-//     std::vector<Point> m_points;
-// 
-//     /**
-//      * @param t_device
-//      *   Id of kinect device.
-//      *
-//      * @param t_config
-//      *   Configuration settings for the kinect device. */
-//     Frame(k4a_device_t& t_device, const uint32_t& t_timeout,
-//         const k4a_image_t& t_xyTable, const k4a_image_t& t_depth,
-//         const k4a_image_t& t_pointcloud);
-// 
-//     void release() const;
-// };
-// #endif /* FRAME_H */
+#ifndef FRAME_H
+#define FRAME_H
+
+#include <k4a/k4a.h>
+#include <opencv2/opencv.hpp> // remove this
+#include <vector>
+
+#include "point.h"
+
+class Frame {
+public:
+    k4a_image_t m_rgb;
+    k4a_image_t m_depth;
+    k4a_image_t m_xyTable;
+    k4a_image_t m_pointcloud;
+    k4a_capture_t m_capture = nullptr;
+
+    std::vector<Point> m_points;
+
+    /**
+     * @param t_device
+     *   Id of kinect device.
+     *
+     * @param t_config
+     *   Configuration settings for the kinect device. */
+    Frame(k4a_device_t& t_device, const uint32_t& t_timeout,
+        const k4a_image_t& t_xyTable, const k4a_image_t& t_depth,
+        const k4a_image_t& t_pointcloud);
+
+    void release() const;
+};
+#endif /* FRAME_H */

d0d4e64ca33168172b206bbb03acbd3fe5f36c4d
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-03-19 09:20:44 +0100

    initial commit

---
+++ include/kinect.h
@@ -0,0 +1,96 @@
+#ifndef KINECT_H
+#define KINECT_H
+
+#include <k4a/k4a.h>
+
+#include "frame.h"
+#include "kexception.h"
+#include "logger.h"
+#include "timer.h"
+
+
+
+struct settings {
+    k4a_device_t m_device = nullptr;
+    static const int32_t TIMEOUT = 1000;
+    k4a_device_configuration_t m_config = K4A_DEVICE_CONFIG_INIT_DISABLE_ALL;
+
+    settings(){
+        m_config.color_format = K4A_IMAGE_FORMAT_COLOR_BGRA32;
+        m_config.color_resolution = K4A_COLOR_RESOLUTION_2160P;
+        m_config.depth_mode = K4A_DEPTH_MODE_NFOV_2X2BINNED;
+        m_config.camera_fps = K4A_FRAMES_PER_SECOND_30;
+        m_config.synchronized_images_only = true;
+    }
+};
+
+
+
+/**
+ * @file kinect.h
+ *    Kinect device.
+ *
+ *    Initializes the kinect device before image capturing.
+ *
+ * @b Reference
+ *    https://github.com/microsoft//tree/develop/examples/fastpointcloud */
+class Kinect {
+private:
+    /**
+     * xyLookupTable
+     *   Pre-computes a lookup table by storing x and y depth sc-
+     *   ale factors for every pixel.
+     *
+     * @param t_calibration
+     *   Calibration for the kinect device in question.
+     *
+     * @param t_depth
+     *   Depth map captured by the kinect device.
+     */
+    static void xyLookupTable(
+        const k4a_calibration_t* t_calibration, k4a_image_t t_depth);
+
+    /**
+     * resolveDepth
+     *   Calibrates point-cloud image resolution based on depth
+     *   image resolution
+     */
+    void resolveDepth();
+
+public:
+    k4a_device_t m_device;
+    k4a_calibration_t m_calibration {};
+
+    k4a_image_t m_xyTable = nullptr;
+    k4a_image_t m_depth = nullptr;
+    k4a_image_t m_pointcloud = nullptr;
+
+    /**
+     * Kinect
+     *
+     * @param t_device
+     *   Id of kinect device.
+     *
+     * @param t_config
+     *   Configuration settings for the kinect device. */
+    Kinect();
+
+    /**
+     * getImage
+     *   Uses kinect to capture an image.
+     *
+     * @param t_timeout
+     *   Timeout window for capturing an image.
+     *
+     * @retval
+     *    Return an Image.
+     */
+    Frame getImage();
+
+    /**
+     * close
+     *   Closes connection to kinect device
+     */
+    void close() const;
+};
+#endif /* KINECT_H */
---
+++ src/kinect.cpp
@@ -0,0 +1,111 @@
+#include "kinect.h"
+
+Kinect::Kinect()
+{
+    Timer timer;
+
+    settings kinect;
+    m_device = kinect.m_device;
+    uint32_t deviceCount = k4a_device_get_installed_count();
+    try {
+        if (deviceCount == 0) {
+            throw kexception();
+        }
+    } catch (kexception& e) {
+        LOG(WARNING) << kexception::findDevice();
+    }
+
+    /* open kinect */
+    try {
+        if (K4A_RESULT_SUCCEEDED
+            != k4a_device_open(K4A_DEVICE_DEFAULT, &m_device)) {
+            throw kexception();
+        }
+    } catch (kexception& e) {
+        LOG(WARNING) << kexception::openDevice();
+    }
+
+    try {
+        if (K4A_RESULT_SUCCEEDED
+            != k4a_device_get_calibration(m_device, kinect.m_config.depth_mode,
+                                          kinect.m_config.color_resolution, &m_calibration)) {
+            throw kexception();
+        }
+    } catch (kexception& e) {
+        LOG(WARNING) << kexception::calibrateDevice();
+    }
+
+    resolveDepth();
+
+    try {
+        if (K4A_RESULT_SUCCEEDED
+            != k4a_device_start_cameras(m_device, &kinect.m_config)) {
+            throw kexception();
+        }
+    } catch (kexception& e) {
+        LOG(WARNING) << kexception::startCamera();
+    }
+    LOG(INFO) << "kinect initialized in: " << timer.getDuration();
+}
+void Kinect::xyLookupTable(
+    const k4a_calibration_t* t_calibration, k4a_image_t t_depth)
+{
+    auto* table_data = (k4a_float2_t*)(void*)k4a_image_get_buffer(t_depth);
+
+    int width = t_calibration->depth_camera_calibration.resolution_width;
+    int height = t_calibration->depth_camera_calibration.resolution_height;
+
+    k4a_float2_t p;
+    k4a_float3_t ray;
+    int valid;
+
+    for (int y = 0, idx = 0; y < height; y++) {
+        p.xy.y = (float)y;
+        for (int x = 0; x < width; x++, idx++) {
+            p.xy.x = (float)x;
+
+            k4a_calibration_2d_to_3d(t_calibration, &p, 1.0F,
+                K4A_CALIBRATION_TYPE_DEPTH, K4A_CALIBRATION_TYPE_DEPTH, &ray,
+                &valid);
+
+            if (valid != 0) {
+                table_data[idx].xy.x = ray.xyz.x;
+                table_data[idx].xy.y = ray.xyz.y;
+            } else {
+                table_data[idx].xy.x = nanf("");
+                table_data[idx].xy.y = nanf("");
+            }
+        }
+    }
+}
+
+void Kinect::resolveDepth()
+{
+    k4a_image_create(K4A_IMAGE_FORMAT_CUSTOM,
+        m_calibration.depth_camera_calibration.resolution_width,
+        m_calibration.depth_camera_calibration.resolution_height,
+        m_calibration.depth_camera_calibration.resolution_width
+            * (int)sizeof(k4a_float2_t),
+        &m_xyTable);
+
+    xyLookupTable(&m_calibration, m_xyTable);
+
+    k4a_image_create(K4A_IMAGE_FORMAT_CUSTOM,
+        m_calibration.depth_camera_calibration.resolution_width,
+        m_calibration.depth_camera_calibration.resolution_height,
+        m_calibration.depth_camera_calibration.resolution_width
+            * (int)sizeof(k4a_float3_t),
+        &m_pointcloud);
+}
+
+Frame Kinect::getImage()
+{
+    return Frame { m_device, settings::TIMEOUT, m_xyTable, m_depth, m_pointcloud };
+}
+
+void Kinect::close() const
+{
+    if (m_device != nullptr) {
+        k4a_device_close(m_device);
+    }
+}

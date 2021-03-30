d5e8f3a91ebdd19548f87423393c5e9d945e14c9
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-03-30 11:20:51 +0200

    revising kinect sense

--- src/kinect.cpp
+++ src/kinect.cpp
@@ -2,9 +2,8 @@
 
 void Kinect::capture()
 {
+    /** capture */
     switch (k4a_device_get_capture(m_device, &m_capture, m_timeout)) {
-
-    /** check if capture is successful */
     case K4A_WAIT_RESULT_SUCCEEDED:
         break;
     case K4A_WAIT_RESULT_TIMEOUT:
@@ -26,10 +25,9 @@ void Kinect::capture()
     }
 }
 
-void Kinect::getPcl(const std::shared_ptr<std::vector<float>>& sptr_points)
+void Kinect::pclImage(
+    const std::shared_ptr<std::vector<float>>& sptr_points) const
 {
-    capture();
-
     /** transform depth image to point cloud */
     if (K4A_RESULT_SUCCEEDED
         != k4a_transformation_depth_image_to_point_cloud(m_transformation,
@@ -37,6 +35,7 @@ void Kinect::getPcl(const std::shared_ptr<std::vector<float>>& sptr_points)
         throw std::runtime_error("Failed to compute point cloud.");
     }
 
+    /** get depth image */
     int width = k4a_image_get_width_pixels(m_pclImage);
     int height = k4a_image_get_height_pixels(m_pclImage);
 
@@ -56,25 +55,67 @@ void Kinect::getPcl(const std::shared_ptr<std::vector<float>>& sptr_points)
     }
 }
 
-void Kinect::getFastPclImage()
-{
-
-    k4a_image_create(K4A_IMAGE_FORMAT_CUSTOM,
-        m_calibration.depth_camera_calibration.resolution_width,
-        m_calibration.depth_camera_calibration.resolution_height,
-        m_calibration.depth_camera_calibration.resolution_width
-            * (int)sizeof(k4a_float2_t),
-        &m_xyTable);
-
-    xyLookupTable(&m_calibration, m_xyTable);
-
-    k4a_image_create(K4A_IMAGE_FORMAT_CUSTOM,
-        m_calibration.depth_camera_calibration.resolution_width,
-        m_calibration.depth_camera_calibration.resolution_height,
-        m_calibration.depth_camera_calibration.resolution_width
-            * (int)sizeof(k4a_float3_t),
-        &m_fastPclImage);
-}
+// void Kinect::pclImage()
+// {
+//     capture();
+//
+//     int depthWidth = k4a_image_get_width_pixels(m_depthImage);
+//     int depthHeight = k4a_image_get_height_pixels(m_depthImage);
+//
+//     k4a_image_create(K4A_IMAGE_FORMAT_CUSTOM,
+//         m_calibration.depth_camera_calibration.resolution_width,
+//         m_calibration.depth_camera_calibration.resolution_height,
+//         m_calibration.depth_camera_calibration.resolution_width
+//             * (int)sizeof(k4a_float2_t),
+//         &m_xyTable);
+//
+//     xyLookupTable(&m_calibration, m_xyTable);
+//
+//     k4a_image_create(K4A_IMAGE_FORMAT_CUSTOM,
+//         m_calibration.depth_camera_calibration.resolution_width,
+//         m_calibration.depth_camera_calibration.resolution_height,
+//         m_calibration.depth_camera_calibration.resolution_width
+//             * (int)sizeof(k4a_float3_t),
+//         &m_fastPclImage);
+//
+//     auto* depthData = (uint16_t*)(void*)k4a_image_get_buffer(m_depthImage);
+//     auto* k4aImageData =
+//     (k4a_float2_t*)(void*)k4a_image_get_buffer(m_xyTable); auto* pclData =
+//     (k4a_float3_t*)(void*)k4a_image_get_buffer(m_fastPclImage);
+//
+//     int numPoints = 0;
+//     for (int i = 0; i < depthWidth * depthHeight; i++) {
+//         if (depthData[i] != 0 && !std::isnan(k4aImageData[i].xy.x)
+//             && !std::isnan(k4aImageData[i].xy.y)) {
+//             pclData[i].xyz.x = k4aImageData[i].xy.x * (float)depthData[i];
+//             pclData[i].xyz.y = k4aImageData[i].xy.y * (float)depthData[i];
+//             pclData[i].xyz.z = (float)depthData[i];
+//             (numPoints)++;
+//         } else {
+//             pclData[i].xyz.x = nanf("");
+//             pclData[i].xyz.y = nanf("");
+//             pclData[i].xyz.z = nanf("");
+//         }
+//     }
+//
+//     int pclWidth = k4a_image_get_width_pixels(m_fastPclImage);
+//     int pclHeight = k4a_image_get_height_pixels(m_fastPclImage);
+//     auto* points =
+//     (k4a_float3_t*)(void*)k4a_image_get_buffer(m_fastPclImage);
+//
+//     for (int i = 0; i < pclWidth * pclHeight; i++) {
+//         /** filter out invalid points */
+//         if (std::isnan(points[i].xyz.x) || std::isnan(points[i].xyz.y)
+//             || std::isnan(points[i].xyz.z)) {
+//             continue;
+//         }
+//         auto x = (float)(points[i].xyz.x);
+//         auto y = (float)(points[i].xyz.y);
+//         auto z = (float)(points[i].xyz.z);
+//         Point point(x, y, z);
+//         m_points.push_back(point);
+//     }
+// }
 
 Kinect::Kinect()
 {
@@ -82,7 +123,7 @@ Kinect::Kinect()
     m_device = deviceConf.m_device;
     m_timeout = deviceConf.TIMEOUT;
 
-    /** open kinect, calibrate device, get transform and start cameras */
+    /** open kinect, calibrate device, start cameras, and get transform */
     uint32_t deviceCount = k4a_device_get_installed_count();
     if (deviceCount == 0) {
         throw std::runtime_error("No device found.");
@@ -96,64 +137,29 @@ Kinect::Kinect()
             deviceConf.m_config.color_resolution, &m_calibration)) {
         throw std::runtime_error("Unable to get calibration.");
     }
-
     m_transformation = k4a_transformation_create(&m_calibration);
+
     if (K4A_RESULT_SUCCEEDED
         != k4a_device_start_cameras(m_device, &deviceConf.m_config)) {
         throw std::runtime_error("Failed to start cameras.");
     }
 
-    /** get capture */
     capture();
 
-    /** get fast point cloud */
-    getFastPclImage();
-
-    /** get non-fast point cloud */
     int depthWidth = k4a_image_get_width_pixels(m_depthImage);
     int depthHeight = k4a_image_get_height_pixels(m_depthImage);
-    m_pclImage = nullptr;
+
+    /** create point cloud image */
     if (K4A_RESULT_SUCCEEDED
         != k4a_image_create(K4A_IMAGE_FORMAT_CUSTOM, depthWidth, depthHeight,
             depthWidth * 3 * (int)sizeof(int16_t), &m_pclImage)) {
         throw std::runtime_error("Failed to create point cloud image.");
     }
+    /** get capture */
+    // capture();
 
-    auto* depthData = (uint16_t*)(void*)k4a_image_get_buffer(m_depthImage);
-    auto* k4aImageData = (k4a_float2_t*)(void*)k4a_image_get_buffer(m_xyTable);
-    auto* pclData = (k4a_float3_t*)(void*)k4a_image_get_buffer(m_fastPclImage);
-
-    int numPoints = 0;
-    for (int i = 0; i < depthWidth * depthHeight; i++) {
-        if (depthData[i] != 0 && !std::isnan(k4aImageData[i].xy.x)
-            && !std::isnan(k4aImageData[i].xy.y)) {
-            pclData[i].xyz.x = k4aImageData[i].xy.x * (float)depthData[i];
-            pclData[i].xyz.y = k4aImageData[i].xy.y * (float)depthData[i];
-            pclData[i].xyz.z = (float)depthData[i];
-            (numPoints)++;
-        } else {
-            pclData[i].xyz.x = nanf("");
-            pclData[i].xyz.y = nanf("");
-            pclData[i].xyz.z = nanf("");
-        }
-    }
-
-    int pclWidth = k4a_image_get_width_pixels(m_fastPclImage);
-    int pclHeight = k4a_image_get_height_pixels(m_fastPclImage);
-    auto* points = (k4a_float3_t*)(void*)k4a_image_get_buffer(m_fastPclImage);
-
-    for (int i = 0; i < pclWidth * pclHeight; i++) {
-        /** filter out invalid points */
-        if (std::isnan(points[i].xyz.x) || std::isnan(points[i].xyz.y)
-            || std::isnan(points[i].xyz.z)) {
-            continue;
-        }
-        auto x = (float)(points[i].xyz.x);
-        auto y = (float)(points[i].xyz.y);
-        auto z = (float)(points[i].xyz.z);
-        Point point(x, y, z);
-        m_points.push_back(point);
-    }
+    // /** get point cloud */
+    // pclImage();
 }
 void Kinect::xyLookupTable(
     const k4a_calibration_t* t_calibration, k4a_image_t t_depth)

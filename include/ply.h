dd3763f1f0c5ae138114578ac03876c9046de58a
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-04-07 18:29:18 +0200

    adapting helper interface

--- include/ply.h
+++ include/ply.h
@@ -1,10 +1,12 @@
 #ifndef PLY_H
 #define PLY_H
 
+#include "point.h"
 #include <k4a/k4a.h>
 
 namespace ply {
-void write(const k4a_image_t& pclImage, const k4a_image_t& rgbImage,
+void write(const Point& lowerBound, const Point& upperBound,
+    const k4a_image_t& pclImage, const k4a_image_t& rgbImage,
     const std::string& file);
 }
 #endif /* PLY_H */

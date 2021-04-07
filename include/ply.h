3ddea79c9696d27d823d0b05d5c462ff96822c75
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-04-07 17:34:12 +0200

    cleaning header

--- include/ply.h
+++ include/ply.h
@@ -4,12 +4,7 @@
 #include <k4a/k4a.h>
 
 namespace ply {
-/** write color write file */
-void write(const k4a_image_t& point_cloud_image, const k4a_image_t& color_image,
-    const char* file_name);
-
-/** downscale images 2x2  binning */
-k4a_image_t downscale(const k4a_image_t& color_image);
-
+void write(const k4a_image_t& pclImage, const k4a_image_t& rgbImage,
+    const std::string& file);
 }
 #endif /* PLY_H */
